2e3b3cbb88694f74fee4df144de8a7a636f24698
Devin Crim <numaniillc@outlook.com>
2020-12-19 02:17:32 -0500

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,87 @@
 //jshint esversion:6
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load
+  
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey)
+
+
+const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
+
+
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _= require("lodash");
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
+const multer = require('multer');
+const path = require('path');
+
+
+
+
+
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+const User = require('./models/user');
+const mongoose =require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+
+
+app.use(session({
+  secret: 'notagoodsecret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+
+
+
+
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+}); 
+
+
+
+app.get('/image', (req, res) => {
+  res.sendFile(__dirname );
+});
+
+// It's very crucial that the file name matches the name attribute in your html
+app.post('/portaits', upload.single('image'), (req, res) => {
+  res.redirect('/');
+});
+
+
+
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/OSSF', {useNewUrlParser:true,  useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+    
+
+
 
 
 app.get("/", function (req, res) {
@@ -16,30 +89,143 @@ app.get("/", function (req, res) {
   
 });
 
-app.get("/about", function (req, res){
-  res.render("about");
-});
-
 
 app.get("/blog", function (req,res){
-  res.render("blog", {startingContent:homeStartingContent});
+  res.render("blog");
     
 });
-
 
 app.get("/shop", function (req,res){
   res.render("shop");
 });
 
-app.get("/contact", function (req,res){
-  res.render("contact");
+app.get("/portraits", function(req,res){
+  res.render("portraits");
+})
+
+
+
+
+app.get('/signup', (req,res)=>{
+  res.render('signup')
+})
+
+
+app.post('/signup', async (req, res) => {
+  const {password, username} = req.body;
+  const hash = await bcrypt.hash (password,12);
+  const user = new User ({
+      username,
+      password: hash
+  })
+  await user.save();
+  req.session.user_id = user._id;
+  res.redirect('/')
+})
+
+
+
+
+
+
+
+app.get("/signin",(req, res)=> {
+  res.render('signin')
+})
+
+
+
+app.post('/signin', async (req, res) => {
+  const {username, password} = req.body;
+  const user = await User.findOne({username});
+ const validPassword = await bcrypt.compare(password, user.password)
+ if(validPassword) {
+     req.session.user_id = user._id;
+     res.redirect('/')
+ }
+ else {
+     res.redirect('/signin')
+ }
+
+})
+
+
+
+app.get("/logout", function (req,res) {
+  res.render("logout");
 });
 
-app.get("/reasons", function (req,res){
-  res.render("reasons");
+app.post('/logout', (req, res) => {
+  req.session.user_id=null;
+  
+  res.redirect('/signin');
+  })
+
+
+
+app.get("/post_04", function (req,res){
+  res.render("post_04");
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/store', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      })
+    }
+  })
+})
+
+
+
+
+app.post('/purchase', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      const itemsJson = JSON.parse(data)
+      const itemsArray = itemsJson.music.concat(itemsJson.merch)
+      let total = 0
+      req.body.items.forEach(function(item) {
+        const itemJson = itemsArray.find(function(i) {
+          return i.id == item.id
+        })
+        total = total + itemJson.price * item.quantity
+      })
+
+      stripe.charges.create({
+        amount: total,
+        source: req.body.stripeTokenId,
+        currency: 'usd'
+      }).then(function() {
+        console.log('Charge Successful')
+        res.json({ message: 'Successfully purchased items' })
+      }).catch(function() {
+        console.log('Charge Fail')
+        res.status(500).end()
+      })
+    }
+  })
+})
 
 
 

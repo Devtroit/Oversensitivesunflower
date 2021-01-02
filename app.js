04caf169e8239d32c44d25a6675efa3ddc7de8af
Devin Crim <numaniillc@outlook.com>
2021-01-02 17:16:11 -0500

    Add app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+
+
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 const formidable = require("formidable");
